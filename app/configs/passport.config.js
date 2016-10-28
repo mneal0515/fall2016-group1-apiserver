4e0e63486488a912abafbca7e05de695f33fb8f6
Apneal <omega_x1_@hotmail.com>
2016-10-28 01:42:24 -0400

    Added passport dependency and configuration

---
+++ app/configs/passport.config.js
@@ -0,0 +1,8 @@
+﻿var passport = require('passport-jwt');
+
+module.exports = exports = {
+    options: {
+        jwtFromRequest: passport.ExtractJwt.fromAuthHeader(),
+        secretOrKey: 'secret'
+    }
+};
